0dfdf9ff2a70b08e1ee8b77a863b8d588d30da6d
mpiniarski <piniarski.m@gmail.com>
2017-10-19 00:41:00 +0200

    Basic configuration

---
+++ client/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
+};
